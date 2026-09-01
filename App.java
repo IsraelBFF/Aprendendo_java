4e08f3d2c9df19346a0a2bdcde05fea58c4e472b
Israel Bernardino <israelbernardino2006@gmail.com>
2026-09-01 14:28:13 -0300

    exercicio

--- App.java
+++ App.java
@@ -2,15 +2,28 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in); 
+        Scanner sc = new Scanner(System.in);
         
-        String nome  = "";
+        System.out.print("Digite um número: ");
+        int n = sc.nextInt();
+        
+        float[] alturas = new float[n];
 
-        System.out.print("Seu nome: ");
-        nome = sc.next();
+        float soma = 0;
 
-        System.out.println(nome);
+        for(int i = 0; i < n; i++){
+            System.out.print("Altura em centímetros: ");
+            alturas[i] = sc.nextFloat();
+            soma += alturas[i];
+        }
+
+        float avg = soma/n;
+        System.out.println();
+
+        System.out.printf("Altura média: %.2f metros\n", avg/100);
         
         sc.close();
+
+
     }
 }
