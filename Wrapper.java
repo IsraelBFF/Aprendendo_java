1ed4ec234be5a63841202f155bab78052a462e62
Israel Bernardino <israelbernardino2006@gmail.com>
2026-09-03 14:31:07 -0300

    wrapper class

---
+++ Wrapper.java
@@ -0,0 +1,31 @@
+public class Wrapper{
+    
+    /* Classes wrapper's são valores do tipo referência que assumem valores similiares
+    ao de um tipo primitivo */
+    /*Exemplos: Integer, Float e String. */
+
+    /* Permite o BOXING e o UNBOXING */
+
+    //BOXING
+
+    // static void main(String[] args){
+    //     int x = 10;
+
+    //     Integer y = x;
+    //     System.out.println(y);
+    // }
+
+    //UNBOXING
+
+    // static void main(String[] args){
+    //     int x = 10;
+
+    //     Integer y = x;
+    //     System.out.println(y);
+
+    //     int z = (int) y;
+
+    //     System.out.println(z);
+    // }
+
+}
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,10 @@
+static void main(String[] args){
+        int x = 10;
+
+        Integer y = x;
+        System.out.println(y);
+
+        int z = (int) y;
+
+        System.out.println(z);
+    }
