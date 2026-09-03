8fd36af48552a9e4f8abc72a1dd82cbf5cba66d8
Israel Bernardino <israelbernardino2006@gmail.com>
2026-09-03 14:41:04 -0300

    for each

---
+++ ForEach.java
@@ -0,0 +1,13 @@
+public class ForEach {
+    static void main(String[] args){
+        // Laço for each
+
+        int[] lista = {1, 2, 3, 4, 5};
+
+        for(int numero : lista){
+            System.out.print(numero + " ");
+        }
+
+        /* Forma menos verbosa de percorrer um vetor */
+    }
+}
