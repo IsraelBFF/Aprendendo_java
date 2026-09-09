b7ec051c75621c0acd523d48105dd6d9de36fbe4
Israel Bernardino <israelbernardino2006@gmail.com>
2026-09-09 14:37:12 -0300

    Date

---
+++ src/application/Data_hora/data.java
@@ -0,0 +1,12 @@
+package application.Data_hora;
+
+import java.util.*;
+
+public class data {
+    static void main(String[] args){
+        Date d1 = new Date();
+
+        System.out.println("d1 = " + d1);
+    }
+    
+}
